83ebd6442c62c8bb3be4203332feef3e5c9b9b50
wuyuehang <yuehang.wu@intel.com>
2014-11-22 22:09:21 +0800

    test

--- browser/android_device.py
+++ browser/android_device.py
@@ -2,7 +2,6 @@ __all__ = ["AndroidDevice"]
 
 from adb_impl import AdbImpl
 from status import *
-from base.log import VLOG
 
 class AndroidDevice(object):
 
@@ -17,18 +16,17 @@ class AndroidDevice(object):
       self.release_callback(self.device_serial)
     return
 
-  def ForwardDevtoolsPort(self, package="", process="", device_socket="", port=""):
+  def ForwardDevtoolsPort(self, package, process, device_socket, port):
     if not device_socket:
       # Assunme this is a WebView app.
       if not process:
-        status, pid = self.xdb.GetPidByName(self.device_serial, package)
+        (status, pid) = self.xdb.GetPidByName(self.device_serial, package)
       else:
-        status, pid = self.xdb.GetPidByName(self.device_serial, process)
+        (status, pid) = self.xdb.GetPidByName(self.device_serial, process)
       if status.IsError():
         if not process:
           status.AddDetails("process name must be specified if not equal to package name")
         return status
-    VLOG(0, "Devtools port: " + port)
     return self.xdb.ForwardPort(self.device_serial, port, package)
          
   def SetUp(self, package, activity, process, args, use_running_app, port):
@@ -37,7 +35,6 @@ class AndroidDevice(object):
     status = self.xdb.CheckAppInstalled(self.device_serial, package)
     if status.IsError():
       return status
-    known_activity = ""
     command_line_file = ""
     device_socket = ""
     exec_name = ""
@@ -45,21 +42,11 @@ class AndroidDevice(object):
       status = self.xdb.ClearAppData(self.device_serial, package)
       if status.IsError():
         return status
-      if known_activity:
-        if activity or process:
-          return Status(kUnknownError, "known package " + package + \
-                      " does not accept activity/process")
-        elif not activity:
-          return Status(kUnknownError, "WebView apps require activity name")
       if command_line_file:
-        status = self.xdb.SetCommandLineFile(self.device_serial, \
-                        exec_name, args)
+        status = self.xdb.SetCommandLineFile(self.device_serial, exec_name, args)
         if status.IsError():
           return status
-      if known_activity:
-        status = self.xdb.Launch(self.device_serial, package, known_activity)
-      else:
-        status = self.xdb.Launch(self.device_serial, package, activity)
+      status = self.xdb.Launch(self.device_serial, package, activity)
       if status.IsError():
         return status
       self.active_package = package
@@ -73,14 +60,4 @@ class AndroidDevice(object):
         return status
       self.active_package = ""
     return Status(kOk)
-   
-if __name__ == "__main__":
-  import time
-  test = AndroidDevice("emulator-5554", AdbImpl(), None)
-  status = Status(kOk)
-  test.SetUp("org.xwalk.simple", ".simpleActivity", "", "", False, "8899")
-  print ">>>>>>>>> SetUp : %s" % status.Message()
-  time.sleep(5)
-  test.TearDown()
-  print ">>>>>>>>> TearDown : %s" % status.Message()
 
--- browser/device_manager.py
+++ browser/device_manager.py
@@ -16,11 +16,10 @@ class DeviceManager(object):
     devices = []
     status = self.xdb.GetDevices(devices)
     if status.IsError():
-      return status, None
+      return (status, None)
     if not devices:
-      return Status(kUnknownError, "There are no devices online"), None
-    status = Status(kUnknownError, "All devices are in use (" + \
-            str(len(devices)) + " online)")
+      return (Status(kUnknownError, "There are no devices online"), None)
+    status = Status(kUnknownError, "All devices are in use (" + str(len(devices)) + " online)")
     for it in devices:
       if self.IsDeviceLocked(it):
         self.active_devices.remove(it)
@@ -31,31 +30,29 @@ class DeviceManager(object):
         device = self.LockDevice(it)
         status = Status(kOk)  
         break
-    #VLOG(0, "acquire device active devices: " + str(self.active_devices))
-    return status, device
+    return (status, device)
 
   # Returns a device with the same guarantees as AcquireDevice, but fails
   # if the device with the given serial number is not avaliable.
   def AcquireSpecificDevice(self, device_serial):
-    VLOG(0, "lol... trapped into AcquireSpecificDevice")
     devices = []
     status = self.xdb.GetDevices(devices)
     if status.IsError():
-      return status, None
+      return (status, None)
     if device_serial not in devices:
-      return Status(kUnknownError, "Device " + device_serial + " is not online"), None
+      return (Status(kUnknownError, "Device " + device_serial + " is not online"), None)
     if self.IsDeviceLocked(device_serial):
-      status = Status(kUnknownError, "Device " + device_serial + " is already in use"), None
+      status = (Status(kUnknownError, "Device " + device_serial + " is already in use"), None)
     else:
       device = self.LockDevice(device_serial)
       status = Status(kOk)
-    return status, device
+    return (status, device)
 
-  def ReleaseDevice(self, device_serial=""):
+  def ReleaseDevice(self, device_serial):
     self.active_devices.remove(device_serial)
     return
 
-  def LockDevice(self, device_serial=""):
+  def LockDevice(self, device_serial):
     self.active_devices.append(device_serial)
     # before following process, xdb'type must be choiced outside
     if isinstance(self.xdb, AdbImpl):
@@ -63,17 +60,6 @@ class DeviceManager(object):
     else:
       return AndroidDevice(device_serial, self.xdb, self.ReleaseDevice)
 
-  def IsDeviceLocked(self, device_serial=""):
+  def IsDeviceLocked(self, device_serial):
     return device_serial in self.active_devices
-
-if __name__ == "__main__":
-  status = Status(kOk)
-  adb = AdbImpl()
-  device = AndroidDevice()
-  dm_test = DeviceManager(adb)
-  status = dm_test.AcquireDevice(device)
-  print ">>>>>>>> AcquireDevice : %s" % status.Message() 
-
-  status = dm_test.AcquireSpecificDevice("emulator-5554", device)
-  print ">>>>>>>> AcquireSpecificDevice : %s" % status.Message() 
 
